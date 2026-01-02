64b2bbad491ae853500ee5125c5e52f12b6daf43
prateeksingh <prateeksingh010101@gmail.com>
2026-01-02 16:34:27 +0530

    3

--- frontend/app.js
+++ frontend/app.js
@@ -8,6 +8,27 @@ form.addEventListener("submit", e => {
   const semester = document.getElementById("semester").value;
   const exam = document.getElementById("exam").value;
   const year = document.getElementById("year").value;
+  const subjectSelect = document.getElementById("subject");
+
+document.getElementById("branch").addEventListener("change", fetchSubjects);
+document.getElementById("semester").addEventListener("change", fetchSubjects);
+
+function fetchSubjects() {
+  const branch = document.getElementById("branch").value;
+  const semester = document.getElementById("semester").value;
+
+  if (!branch || !semester) return;
+
+  fetch(`http://localhost:5000/api/papers/subjects?branch=${branch}&semester=${semester}`)
+    .then(res => res.json())
+    .then(subjects => {
+      subjectSelect.innerHTML = `<option value="">Select Subject</option>`;
+      subjects.forEach(sub => {
+        subjectSelect.innerHTML += `<option value="${sub}">${sub}</option>`;
+      });
+    });
+}
+
 
   fetch(`http://localhost:5000/api/papers?branch=${branch}&semester=${semester}&exam=${exam}&year=${year}`)
     .then(res => res.json())
